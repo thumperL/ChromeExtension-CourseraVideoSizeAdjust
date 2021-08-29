9ec0e1349e8e4fb43e7ec4249c0d3f31fe9c9e25
Thumper <shaunwhl1991@gmail.com>
2021-08-29 19:19:41 +1000

    Update manifest to force on Coursera class page only
    Signed-off-by: Thumper <shaunwhl1991@gmail.com>

--- content_adjust.js
+++ content_adjust.js
@@ -2,15 +2,15 @@ window.onload = () => {
   // Observe document to find if rc-VideoControlsContainer has been added to DOM
   MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
   const observer = new MutationObserver(function(mutations, observer) {
-      if (document.querySelector('.rc-VideoControlsContainer') !== null) {
-        document.querySelector('.rc-VideoControlsContainer').addEventListener('click', () => {
-          // Once click on the play video button, adjust the floating video size.
-          // For some reason manipulating body DOM will screw up the layout when video is not yet played
-          chrome.storage.sync.get(['width'], (result) => chrome.runtime.sendMessage({'width': `${result.width}`}));
-        });
-        // Once it is there, hook it on and disconnect the observer
-        observer.disconnect();
-      }
+    if (document.querySelector('.rc-VideoControlsContainer') !== null) {
+      document.querySelector('.rc-VideoControlsContainer').addEventListener('click', () => {
+        // Once click on the play video button, adjust the floating video size.
+        // For some reason manipulating body DOM will screw up the layout when video is not yet played
+        chrome.storage.sync.get(['width'], (result) => chrome.runtime.sendMessage({'width': `${result.width}`}));
+      });
+      // Once it is there, hook it on and disconnect the observer
+      observer.disconnect();
+    }
   });
   observer.observe(document, {subtree: true,attributes: true});
 };
