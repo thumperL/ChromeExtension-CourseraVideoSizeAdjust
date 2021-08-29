d221d97a387513625c0d6b112b08906be932f8eb
Thumper <shaunwhl1991@gmail.com>
2021-08-29 18:41:41 +1000

    Refactor onInstalled block and update storage on executeScript

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({width: '300px'}, () => {
-    console.log('Default width is 300px.');
+chrome.runtime.onInstalled.addListener(() => chrome.storage.sync.set({width: '300px'}));
+
+chrome.runtime.onMessage.addListener(async (request) => {  
+  // Add the style tag in with ID to avoid keep on adding the tag to DOM
+  chrome.tabs.executeScript({
+    code: `
+    if (document.querySelector('style#coursera_video_width_style_tag') === null) {
+      document.body.insertAdjacentHTML('afterend', '<style id="coursera_video_width_style_tag">.rc-VideoMiniPlayer.mini .rc-VideoMiniControls, .rc-VideoMiniPlayer.mini .video-main-player-container {width: ${request.width}%;}</style>');
+    } else {
+      document.querySelector('style#coursera_video_width_style_tag').innerHTML = ".rc-VideoMiniPlayer.mini .rc-VideoMiniControls, .rc-VideoMiniPlayer.mini .video-main-player-container {width: ${request.width}%;}";
+    }`
   });
-chrome.runtime.onMessage.addListener(async (request) => {
-    chrome.tabs.executeScript({
-      code: `
-      if (document.querySelector('style#coursera_video_width_style_tag') === null) {
-        document.body.innerHTML += '<style id="coursera_video_width_style_tag">.rc-VideoMiniPlayer.mini .rc-VideoMiniControls, .rc-VideoMiniPlayer.mini .video-main-player-container {width: ${request.width}%;}</style>';
-      } else {
-        document.querySelector('style#coursera_video_width_style_tag').innerHTML = ".rc-VideoMiniPlayer.mini .rc-VideoMiniControls, .rc-VideoMiniPlayer.mini .video-main-player-container {width: ${request.width}%;}";
-      }`
-    });
-  }
-);
+  // Update the saved width
+  chrome.storage.sync.set({width: `${request.width}`});
+});
