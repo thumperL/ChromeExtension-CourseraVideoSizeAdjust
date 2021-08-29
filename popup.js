b4f43900895a2bf91ee1390c00a6e52033411ea5
Thumper <shaunwhl1991@gmail.com>
2021-08-29 18:45:52 +1000

    Add default popup display, refactor videoWidthSelector change listener, and add current percent display

--- popup.js
+++ popup.js
@@ -1,7 +1,15 @@
-var videoWidthSelector = document.querySelector('#videoWidthSelector');
+const videoWidthSelector = document.querySelector('#videoWidthSelector');
+const videoWidthSelectorValue = document.querySelector('#videoWidthSelectorValue');
+
 videoWidthSelector.addEventListener('change', (e) => {
-  chrome.storage.sync.set({width: e.target.value});
-  chrome.runtime.sendMessage({'width': `${e.target.value}`}, function(response) {
-    console.log('response', response);
+  videoWidthSelectorValue.innerHTML = `${e.target.value}%`;
+  chrome.runtime.sendMessage({'width': `${e.target.value}`});
+});
+
+window.onload = async function() {
+  await chrome.storage.sync.get(['width'], async (result) => {
+    // Set default draggable window
+    videoWidthSelector.value = result.width;
+    videoWidthSelectorValue.innerHTML = (result.width.endsWith('px')) ? `${result.width}` : `${result.width}%`;
   });
-})
+};
